ed98d2582524805ff9a5ad703fabc4255c2b2683
ZavaleevA <86657451+ZavaleevA@users.noreply.github.com>
2025-01-29 19:20:34 +0200

    Update is_legitimate_ip.php

--- is_legitimate_ip.php
+++ is_legitimate_ip.php
@@ -5,23 +5,12 @@ function isLegitimateIp($ip) {
     $apiKey = $config['ipqualityscore']; // Specify your API key
     $url = "https://www.ipqualityscore.com/api/json/ip/$apiKey/$ip";
     
-    $ch = curl_init();
-    
-    // Set cURL options
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirects if any
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Disable SSL verification (if necessary)
-
-    $response = curl_exec($ch);
-    curl_close($ch);
-
+    $response = file_get_contents($url);
     if ($response === false) {
         return false; // API error
     }
 
     $data = json_decode($response, true);
-
     if ($data['success'] && $data['fraud_score'] < 75 && !$data['proxy'] && !$data['vpn']) {
         return true; // Legitimate IP
     }
