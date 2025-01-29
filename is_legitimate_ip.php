077c5e054e06a3fb1550fb03e40a5fdf58dc179b
ZavaleevA <86657451+ZavaleevA@users.noreply.github.com>
2025-01-29 18:06:56 +0200

    Update is_legitimate_ip.php

--- is_legitimate_ip.php
+++ is_legitimate_ip.php
@@ -7,18 +7,14 @@ function isLegitimateIp($ip) {
     
     $response = file_get_contents($url);
     if ($response === false) {
-        echo '4';
-        exit();
         return false; // API error
     }
-    echo '1';
+
     $data = json_decode($response, true);
+    echo $data;
+    exit();
     if ($data['success'] && $data['fraud_score'] < 75 && !$data['proxy']) {
-        echo '2';
-        exit();
         return true; // Legitimate IP
     }
-    echo '3';
-    exit();
     return false; // Suspicious or proxy IP
 }
